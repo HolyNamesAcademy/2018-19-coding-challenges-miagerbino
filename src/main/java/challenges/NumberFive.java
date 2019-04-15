afc997ab27cbb9db4b0e1bb58ac098a5127a352f
miagerbino <miagerbino@gmail.com>
2019-04-15 07:58:29 -0700

    Working on test 5, 1-4 finished

--- src/main/java/challenges/NumberFive.java
+++ src/main/java/challenges/NumberFive.java
@@ -32,26 +32,37 @@ public class NumberFive {
     public boolean isAnagram(String word1, String word2) {
         // Delete the line below and implement the method!
         //throw new UnsupportedOperationException();
+        word1.trim();
+        word2.trim();
         if(word1.length()!=word2.length()){
             return false;
         }
         ArrayList <Character> word1letters = new ArrayList <Character>();
         ArrayList <Character> word2letters = new ArrayList <Character>();
+
+        //load arrays with characters of strings
         for(int i = 0; i<word1.length(); i++){
             word1letters.add(word1.charAt(i));
             word2letters.add(word2.charAt(i));
         }
+
+        //check each character against the other word
+        int a = 0;
+        int b = 0;
         for (int i = 0; i<word1letters.size(); i++){
             for (int j = 0; j<word1letters.size(); j++){
-                if(word1letters.get(i).equals(word2letters.get(j))){
-                    word1letters.remove(i);
-                    word2letters.remove(j);
-                    i--;
-                    j--;
+                if(word1letters.get(a).equals(word2letters.get(b))){
+                    word1letters.remove(a);
+                    word2letters.remove(b);
+                    a--;
+                    b--;
                 }
+            break;
             }
+        a++;
+        b++;
         }
-        if(word1letters.isEmpty()){
+        if(word1letters.isEmpty()&&word2letters.isEmpty()){
             return true;
         }
         return false;
